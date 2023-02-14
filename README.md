a4bebcece886c0b7d4b94f6706d2aecad1697e18
meowmeowxw <giovanni.disanti@protonmail.com>
2023-02-14 22:26:58 +0100

    Add cli parameters with colors mode/top/rgb

--- README.md
+++ README.md
@@ -9,6 +9,17 @@ $ gcc main.c -o main -I/usr/include/hidapi -lhidapi-libusb
 
 ## Run
 
+I don't follow standards and I don't have time to implement a proper help atm 🤣.
+
 ```
-$ ./main
+$ ./main -t [top|bottom] -r <rgb hex value> -mode [fixed|breathing]
 ```
+
+Example:
+
+```
+sudo ./main -t top -r fc200c -m fixed
+```
+
+To work without sudo you need to add an udev rule as documented in https://github.com/libusb/hidapi#hidapi-has-four-back-ends
+
--- main.c
+++ main.c
@@ -1,24 +1,80 @@
-#include <stdio.h>
+#include <ctype.h>
 #include <hidapi.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_STR 255
 
-int main(int argc, char* argv[])
-{
+void print_payload(uint8_t *ptr, size_t size) {
+    for (int i = 0; i < size; i++) {
+        printf("0x%02x ", *(ptr + i));
+        if (i % 8 == 7) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+int main(int argc, char* argv[]) {
     int res;
     unsigned char buf[65];
     wchar_t wstr[MAX_STR];
     hid_device *handle;
     int i;
+    int arg;
+    int opt;
+    uint32_t len = 0;
+
+    uint8_t color_mode = 0x01;
+    uint8_t top = 0x00;
+    uint32_t rgb = 0xf74433;
+
+      
+    while((opt = getopt(argc, argv, ":r:t:m:")) != -1) 
+    { 
+        switch(opt) 
+        { 
+            case 't': 
+                if (!strcmp("top", optarg)) {
+                    top = 0x01;
+                }
+                printf("top: %d\n", top);
+                break; 
+            case 'r': 
+                len = strlen(optarg);
+                if (len != 6) {
+                    printf("provide rgb in 6 hex characters, example: ff0404\n");
+                    exit(1);
+                }
+                rgb = (int)strtol(optarg, NULL, 16);
+                printf("rgb: %06x\n", rgb);
+                break; 
+            case 'm':
+                if (!strcmp("breathing", optarg)) {
+                    color_mode = 0x02;
+                }
+                break;
+        } 
+    }
+
+    uint8_t payload[] = "\x11\xff\x04\x3a\x01\x01\xff\xff\xff\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+    print_payload(payload, 20);
+    payload[4] = top;
+    payload[5] = color_mode;
+    payload[6] = (rgb >> 16) & 0xff;
+    payload[7] = (rgb >> 8) & 0xff;
+    payload[8] = rgb & 0xff;
+    print_payload(payload, 20);
 
     // Initialize the hidapi library
     res = hid_init();
 
     // Open the device using the VID, PID,
     // and optionally the Serial number.
+    // Get the data executing `lsusb` command
     handle = hid_open(0x46d, 0xab5, NULL);
     if (!handle) {
         printf("Unable to open device\n");
@@ -36,22 +92,20 @@ int main(int argc, char* argv[])
 
     // Read the Serial Number String
     res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-    printf("Serial Number String: (%d) %ls\n", wstr[0], wstr);
+    printf("Serial Number String: (%d) %ls", wstr[0], wstr);
 
-    // Read Indexed String 1
-    //
-    res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
-    printf("Indexed String 1: %ls\n", wstr);
-
-    // Toggle LED (cmd 0x80). The first byte is the report number (0x0).
-    uint8_t *x = "\x11\xff\x04\x3a\x01\x01\x03\x01\x21\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
-    memcpy(buf, x, 27);
-    printf("%d\n", *(uint8_t *)buf);
-    res = hid_write(handle, buf, 20);
-    printf("%d\n", res);
+    res = hid_write(handle, payload, 20);
+    printf("\nwrite %d bytes\n", res);
 
     hid_close(handle);
     hid_exit();
 
     return 0;
 }
+
+// "\x11\xff\x04\x3a\x01\x03\x00\x00\x00\x00\x00\x13\x88\x64\x00\x00\x00\x00\x00\x00" // cycle
+// "\x11\xff\x04\xda\x01\x08\x13\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" // breathing
+// "\x11\xff\x04\x3a\x01\x02\x00\xff\xff\x13\x88\x00\x64\x00\x00\x00\x00\x00\x00\x00"
+
+// "\x11\xff\x04\x3a\x01\x02\x00\x61\x61\x13\x88\x00\x64\x00\x00\x00\x00\x00\x00\x00" // breathing
+// "\x11\xff\x04\x3a\x01\x01\x00\x61\x61\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" // fixed
